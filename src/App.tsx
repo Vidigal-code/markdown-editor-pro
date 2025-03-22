4aa81776b9e719833149b6a534a9b2097d62ad39
Kauan Vidigal <kauanvidigalcontato@gmail.com>
2025-03-22 01:54:44 -0300

    fix gh pages hash router #

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import {Routes, Route} from 'react-router-dom';
-import {Presentation} from "./components/presentation/Presentation.tsx";
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Presentation } from "./components/presentation/Presentation.tsx";
 import Render from "./Render.tsx";
-
 
 export const App = () => {
     return (
-        <Routes>
-            <Route path="/" element={<Presentation/>}/>
-            <Route path="/render" element={<Render/>}/>
-        </Routes>
+        <HashRouter>
+            <Routes>
+                <Route path="/" element={<Presentation />} />
+                <Route path="/render" element={<Render />} />
+            </Routes>
+        </HashRouter>
     );
 };
